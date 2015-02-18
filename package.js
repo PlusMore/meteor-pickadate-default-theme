0343f30dbbbdcb6d7eddfc112341b827b712ee1a
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-18 14:59:51 -0500

    update package.js

--- package.js
+++ package.js
@@ -1,5 +1,10 @@
 Package.describe({
-  summary: "The mobile-friendly, responsive, and lightweight jQuery date & time input picker."
+  summary: "Default theme for the mobile-friendly, responsive, and lightweight jQuery date & time input picker."
+  version: "3.3.1",
+  // Optional.  Default is package directory name.
+  name: "plusmore:pickadate-default-theme",
+  // Optional github URL to your source repository.
+  git: "https://github.com/PlusMore/meteor-pickadate-default-theme.git"
 });
 
 Package.on_use(function (api, where) {
